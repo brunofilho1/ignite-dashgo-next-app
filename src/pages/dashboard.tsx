2a28df2fdbddb79ce0732cdeb54b59468b663644
Bruno Filho <bruno@publi.com.br>
2022-05-20 12:08:56 -0300

    inicio da sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,15 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
-  return <Header />;
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 }
