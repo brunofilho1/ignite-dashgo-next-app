a2f0a1f2c819e8e854f0efd8a4003c1cb3848730
Bruno Filho <bruno@publi.com.br>
2022-05-25 11:47:49 -0300

    created users page with a chakra-ui table

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,88 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justifyContent="space-between" alignContent="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo usuário
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" w="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Bruno Filho</Text>
+                    <Text fontWeight="normal" fontSize="sm" color="gray.300">
+                      bruno@publi.com.br
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>25 de maio de 2022</Td>
+                <Td>
+                  <Button
+                    title="Editar"
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                  >
+                    <Icon as={RiPencilLine} fontSize="16" />
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
