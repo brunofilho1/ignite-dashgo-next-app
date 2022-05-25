09bfb978a1371a687c7ff2a30e1b95136ff9b47c
Bruno Filho <bruno@publi.com.br>
2022-05-25 11:20:42 -0300

    Fixed apexcharts error and added 2 charts

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,19 +1,61 @@
-import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
-import Chart from "react-apexcharts";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+const Chart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
 
 export default function Dashboard() {
   const options = {
     chart: {
-      id: "basic-bar",
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+      foreColor: theme.colors.gray[500],
     },
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+    grid: {
+      show: false,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    tootip: {
+      enabled: false,
+    },
+    xasis: {
+      type: "datetime",
+      axisBorder: {
+        color: theme.colors.gray[600],
+      },
+      axisTicks: {
+        color: theme.colors.gray[600],
+      },
+      categories: [
+        "2022-05-25T11:15:56.000Z",
+        "2022-05-26T11:15:56.000Z",
+        "2022-05-27T22:25:56.000Z",
+        "2022-05-28T13:04:56.000Z",
+        "2022-05-29T11:15:56.000Z",
+        "2022-05-30T01:15:56.000Z",
+        "2022-05-31T11:15:56.000Z",
+      ],
+    },
+    fill: {
+      opacity: 0.3,
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        opacityFrom: 0.7,
+        opacityTo: 0.3,
+      },
     },
   };
 
-  const series = [{ name: "series1", data: [31, 120, 10, 28, 24, 204] }];
+  const series = [{ name: "series1", data: [101, 120, 10, 208, 154, 204] }];
 
   return (
     <Flex direction="column" h="100vh">
@@ -22,7 +64,12 @@ export default function Dashboard() {
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <Sidebar />
 
-        <SimpleGrid flex="1" gap="4" minChildWidth="320px">
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          alignContent="flex-start"
+        >
           <Box p="8" bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
@@ -39,6 +86,13 @@ export default function Dashboard() {
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
+            <Chart
+              type="area"
+              width="500"
+              height="160"
+              options={options}
+              series={series}
+            />
           </Box>
         </SimpleGrid>
       </Flex>
