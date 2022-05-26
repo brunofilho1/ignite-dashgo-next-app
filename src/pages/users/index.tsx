f2c00869a7a93a8a3c3f73a7488544600fb82954
Bruno Filho <oliveira.brunofilho@gmail.com>
2022-05-26 11:41:21 -0300

    create user page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -80,6 +80,31 @@ export default function UserList() {
                   </Button>
                 </Td>
               </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Bruno Filho</Text>
+                    <Text fontWeight="normal" fontSize="sm" color="gray.300">
+                      bruno@publi.com.br
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>25 de maio de 2022</Td>
+                <Td>
+                  <Button
+                    title="Editar"
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                  >
+                    <Icon as={RiPencilLine} fontSize="16" />
+                  </Button>
+                </Td>
+              </Tr>
             </Tbody>
           </Table>
           <Pagination />
