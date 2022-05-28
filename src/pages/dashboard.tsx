9587bf162e356275050e85b5c4d72652a849654c
Bruno Filho <oliveira.brunofilho@gmail.com>
2022-05-28 10:52:09 -0300

    fully responsive dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -70,25 +70,25 @@ export default function Dashboard() {
           minChildWidth="320px"
           alignContent="flex-start"
         >
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
             <Chart
               type="area"
-              width="500"
+              width="100%"
               height="160"
               options={options}
               series={series}
             />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
             <Chart
               type="area"
-              width="500"
+              width="100%"
               height="160"
               options={options}
               series={series}
