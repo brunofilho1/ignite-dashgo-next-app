a88c01de9c4c395fd8216277c60770b32503d42e
Bruno Filho <oliveira.brunofilho@gmail.com>
2022-06-10 10:26:14 -0300

    turning getUsers function into a hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -20,32 +20,10 @@ import { RiAddLine, RiPencilLine, RiRefreshLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { useQuery } from "react-query";
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error, refetch } = useQuery(
-    "users",
-    async () => {
-      const { data } = await api.get("/users");
-
-      const users = data.users.map((user) => ({
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      }));
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 5,
-    }
-  );
+  const { data, isLoading, isFetching, error, refetch } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
