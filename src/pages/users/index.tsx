b4b8077f0319e9d5faedc1c0f167a1b1e1e4e7eb
Bruno Filho <oliveira.brunofilho@gmail.com>
2022-06-10 10:16:38 -0300

    swapping fetch for axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,13 +21,13 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { useQuery } from "react-query";
+import { api } from "../../services/api";
 
 export default function UserList() {
   const { data, isLoading, isFetching, error, refetch } = useQuery(
     "users",
     async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
+      const { data } = await api.get("/users");
 
       const users = data.users.map((user) => ({
         id: user.id,
