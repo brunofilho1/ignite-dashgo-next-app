cf7f7fc6adc2c5b73e39c7d223c91f14ae30fedb
Bruno Filho <oliveira.brunofilho@gmail.com>
2022-06-10 10:48:59 -0300

    pagination logic on miragejs

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from "miragejs";
+import { createServer, Factory, Model, Response } from "miragejs";
 import { faker } from "@faker-js/faker";
 
 type User = {
@@ -30,14 +30,37 @@ export function makeServer() {
 
     // gerar dados ao iniciar
     seeds(server) {
-      server.createList("user", 10);
+      server.createList("user", 200);
     },
 
     routes() {
       this.namespace = "api";
       this.timing = 750;
 
-      this.get("/users");
+      this.get("/users", function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const totalItems = schema.all("user").length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        console.log(request);
+        console.log(page, per_page);
+        console.log(pageStart);
+        console.log(pageEnd);
+
+        return new Response(
+          200,
+          { "x-total-count": String(totalItems) },
+          { users }
+        );
+      });
       this.post("/users");
 
       this.namespace = "";
